eff28ba224494cf51c3c91e257f169e59a95655f
Yurii Demchenko <djdomovoy@gmail.com>
2023-09-15 18:23:20 +0300

    Fixes

--- modules/ui/page_objects/sign_in_tt_page.py
+++ modules/ui/page_objects/sign_in_tt_page.py
@@ -1,6 +1,5 @@
 from time import sleep
 from modules.ui.page_objects.base_tt import Base
-from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,54 +9,65 @@ from selenium.common.exceptions import NoSuchElementException
 
 class SignInPage(Base):
     URL = "https://thinking-tester-contact-list.herokuapp.com/"
+    CREDENTIALS = {"email": "d1@example.com", "pass": "1234567"}
 
     def __init__(self):
+        # Call the constructor of the Base class and initialize the WebDriverWait instance
         super().__init__()
         self.wait = WebDriverWait(
             self.driver, 10, poll_frequency=1, ignored_exceptions=NoSuchElementException
         )
 
     def go_to(self):
+        # Navigate to the URL
         self.driver.get(SignInPage.URL)
 
-    def try_login(self, username, password):
-        login_elem = self.driver.find_element(By.ID, "email")
-        login_elem.send_keys(username)
-        pass_elem = self.driver.find_element(By.ID, "password")
-        pass_elem.send_keys(password)
+    def login(self):
+        # Find the login elements, input the credentials and click the submit
+        email_input = self.driver.find_element(By.ID, "email")
+        pass_input = self.driver.find_element(By.ID, "password")
         submit_button = self.driver.find_element(By.ID, "submit")
+
+        email_input.send_keys(self.CREDENTIALS["email"])
+        pass_input.send_keys(self.CREDENTIALS["pass"])
         submit_button.click()
 
     def logout(self):
+        # Find the logout button and click it
         logout_button = self.wait.until(
             EC.presence_of_element_located((By.ID, "logout"))
         )
         logout_button.click()
 
-    def open_first_contact(self):
+    def open_contact(self, contact_number):
+        # Find the contact element by its number and click it to open the contact details
         try:
             element = self.wait.until(
                 EC.presence_of_element_located(
-                    (By.XPATH, "//*[@id='myTable']/tr/td[2]")
+                    (By.XPATH, f"//*[@id='myTable']/tr/td[{contact_number + 1}]")
                 )
             )
             element.click()
+        except NoSuchElementException as e:
+            print(f"Element not found: {e}")
         except Exception as e:
-            print(e)
+            print(f"An error occurred: {e}")
 
-    def first_contact_edit(
+    def contact_edit(
         self,
         first_name,
         last_name,
         city,
+        dob,
+        email,
     ):
-        # Enter edit form
+        # Click the edit button to enter edit mode for the contact
         edit = self.driver.find_element(By.ID, "edit-contact")
         edit.click()
 
         sleep(0.5)  # temp solution
 
-        # locators
+        # Find all necessary input fields and the submit button using their locators
         first_name_input = self.wait.until(
             EC.presence_of_element_located((By.ID, "firstName"))
         )
@@ -65,12 +75,16 @@ class SignInPage(Base):
             EC.presence_of_element_located((By.ID, "lastName"))
         )
         city_input = self.wait.until(EC.presence_of_element_located((By.ID, "city")))
+        dob_input = self.wait.until(
+            EC.presence_of_element_located((By.ID, "birthdate"))
+        )
+        email_input = self.wait.until(EC.presence_of_element_located((By.ID, "email")))
+
         submit_button = self.wait.until(
             EC.presence_of_element_located((By.ID, "submit"))
         )
 
-        # Actions
-        first_name_input.clear()
+        # Clear the existing value and set the new value for the input field
         first_name_input.send_keys(Keys.CONTROL + "a")
         first_name_input.send_keys(Keys.DELETE)
         first_name_input.send_keys(first_name)
@@ -83,5 +97,13 @@ class SignInPage(Base):
         city_input.send_keys(Keys.DELETE)
         city_input.send_keys(city)
 
-        # Press submit button
+        dob_input.send_keys(Keys.CONTROL + "a")
+        dob_input.send_keys(Keys.DELETE)
+        dob_input.send_keys(dob)
+
+        email_input.send_keys(Keys.CONTROL + "a")
+        email_input.send_keys(Keys.DELETE)
+        email_input.send_keys(email)
+
+        # Click the submit button to submit the form
         submit_button.click()
--- tests/ui/test_ui_tt_page_object.py
+++ tests/ui/test_ui_tt_page_object.py
@@ -1,26 +1,31 @@
-# from modules.ui.page_objects.sign_in_tt_page import SignInPage
 import pytest
 
 
 @pytest.mark.uitt
 def test_login(thinkingTester_ui):
     thinkingTester_ui.go_to()
-    thinkingTester_ui.try_login("d1@example.com", "1234567")
+    thinkingTester_ui.login()
     thinkingTester_ui.logout()
 
 
 @pytest.mark.uitt
 def test_open_first_contact(thinkingTester_ui):
     thinkingTester_ui.go_to()
-    thinkingTester_ui.try_login("d1@example.com", "1234567")
-    thinkingTester_ui.open_first_contact()
+    thinkingTester_ui.login()
+    thinkingTester_ui.open_contact(1)
     thinkingTester_ui.logout()
 
 
 @pytest.mark.uitt
 def test_edit_first_contact(thinkingTester_ui):
     thinkingTester_ui.go_to()
-    thinkingTester_ui.try_login("d1@example.com", "1234567")
-    thinkingTester_ui.open_first_contact()
-    thinkingTester_ui.first_contact_edit("John", "Doe", "Nebraska")
+    thinkingTester_ui.login()
+    thinkingTester_ui.open_contact(1)
+    thinkingTester_ui.contact_edit(
+        "Garry",
+        "Jeil",
+        "New York",
+        "1985-11-01",
+        "g.j@mail.com",
+    )
     thinkingTester_ui.logout()
